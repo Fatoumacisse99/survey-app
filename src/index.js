1e16aa38abf05694708d1a58fba819fb2b2819fb
Fatoumacisse99 <cfama673@gmail.com>
2024-08-19 01:38:13 +0200

    reajustement

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const responseModule = require("./module/responseModule");
 
 const newSurvey = {
   id: 1,
-  name: "Document 101",
+  name: "Document 2",
   description: "Ceci est un document101 important",
   createdAt: new Date(),
   createdBy: {
@@ -13,57 +13,63 @@ const newSurvey = {
   },
 };
 const updateSurvey = {
-  name: "Document 101- Mise à jour",
+  name: "Document 2- Mise à jour",
   description: "Ceci est un document101 important mis à jour",
   createdAt: new Date(),
   createdBy: {
-    employeeName: "Ahmed Mouhamed",
-    employeeRole: "Developpeur",
+    employeeName: "Mariame",
+    employeeRole: "Developpeuse",
   },
 };
 const newQuestion = {
-  id: 2,
-  idSurvey: 101,
+  id: 4,
+  idSurvey: 2,
   title: "Quel est le titre du document2 ?",
   type: "texte",
-  options: 2,
-};
+  options: {
+    minValue : 1,
+    maxValue : 4,
+    step : 1,}}
+
 const updateQuestion = {
   id: 2,
-  idSurvey: 4000,
+  idSurvey: 1,
   title: "Quel est le titre mis à jour du document2 ?",
   type: "texte",
-  options: 5,
+  options: {
+    minValue : 1,
+    maxValue : 4,
+    step : 1,}
 };
 
 const newResponse = {
-  id: 2,
-  idQuestion: 6,
+  id: 1,
+  idQuestion: 1,
   title: "Réponse à la question2",
   type: "texte",
   
 };
 const updateResponse = {
-  idQuestion: 10,
+  idQuestion: 1,
   title: "Réponse mise à jour à la question2",
   type: "texte",
 };
 async function main() {
   try {
     await surveyModule.addSurvey(newSurvey);
-    await surveyModule.updateSurvey(1, updateSurvey);
+    await surveyModule.updateSurvey(2, updateSurvey);
     await surveyModule.findSurvey();
-    await surveyModule.deleteSurvey(100);
+    await surveyModule.deleteSurvey(1);
 
     await questionModule.addQuestion(newQuestion);
-    await questionModule.updateQuestion(45, updateQuestion);
+    await questionModule.updateQuestion(3, updateQuestion);
     await questionModule.findQuestion();
-    await questionModule.deleteQuestion(100);
+    await questionModule.deleteQuestion(1);
 
     await responseModule.addResponse(newResponse);
-    await responseModule.updateResponse(10, updateResponse);
+    await responseModule.updateResponse(1, updateResponse);
     await responseModule.findResponse();
-    await responseModule.deleteResponse(11);
+    await responseModule.deleteResponse(1);
   } catch (error) {
     console.error("Erreur lors de l'exécution:", error.message);
   }
